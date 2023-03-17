60aad8ef5406f37983933f2ca8508dcb08774153
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-17 16:18:59 -0400

    some more data types

--- src/data.rs
+++ src/data.rs
@@ -5,13 +5,64 @@ use crate::{Deserialize, Serialize};
 
 #[wasm_bindgen]
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct CurrentLight {
-    pub on: bool,
+pub struct CameraFrame {
+    pub image: Vec<u8>,
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct DutyCycle {
+    pub duty_cycle: f64,
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct IntruderAlert {
+    pub image: Vec<u8>,
+    pub timestamp: String,
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct LightEffectDesiredBrightness {
+    lumens: u32,
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct LightEffectForceEndBrightness {
+    lumens: u32,
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub enum LightEffect {
+    LightEffectDesiredBrightness(LightEffectDesiredBrightness),
+    LightEffectForceEndBrightness(LightEffectForceEndBrightness),
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Timer {
+    effect: LightEffect,
+    weekday: u8,
+    hour: u8,
+    minute: u8,
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Preferences {
+    name: String,
+    timers: Vec<Timer>,
 }
 
 // TODO: https://rustwasm.github.io/docs/wasm-bindgen/reference/attributes/on-rust-exports/typescript_custom_section.html
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 // #[serde(tag = "variant", content = "data")]
 pub enum Data {
-    CurrentLight(CurrentLight),
+    CameraFrame(CameraFrame),
+    DutyCycle(DutyCycle),
+    IntruderAlert(IntruderAlert),
+    Preferences(Preferences),
 }
